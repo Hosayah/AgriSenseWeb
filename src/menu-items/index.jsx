32df96acd082dde82397c5e6c2ebd6fb964326b5
Hosayah <catabayjosiah19@gmail.com>
2025-12-27 16:58:04 +0800

    haha

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -3,6 +3,7 @@ import farmOwner from './farmOwner';
 import admin from './admin';
 import pages from './page';
 import { useAuth } from 'contexts/AuthContext';
+import { Link as RouterLink, useNavigate  } from 'react-router-dom';
 
 // ==============================|| MENU ITEMS ||============================== //
 
@@ -10,11 +11,12 @@ function RoleBasedMenu() {
   const { user } = useAuth();
   console.log("RoleBasedMenu user role:", user?.role); // <- check role
 
+  if(!user) return pages; 
   if (user?.role === "admin") return admin;
   return farmOwner;
 }
 
-const menu = RoleBasedMenu() ? RoleBasedMenu() : farmOwner;
+const menu = RoleBasedMenu();
 
 const menuItems = {
   items: [menu, pages]
